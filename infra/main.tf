bd6c2bf26a8ec1c4d2604936fbc683a05e0058d4
spintaglobal <spinta.global@gmail.com>
2026-02-26 13:52:12 +0100

    Updated code for portfolio

--- infra/main.tf
+++ infra/main.tf
@@ -1,36 +1,29 @@
 terraform {
   required_providers {
     netlify = {
-      source = "netlify/netlify"
-      version = "0.6.1"
+      source  = "sry270/netlify"
+      version = "0.1.1"
     }
   }
 }
 
 provider "netlify" {
-  # We will pass the token via environment variable for security
+  # We will use the NETLIFY_TOKEN environment variable
   # export NETLIFY_TOKEN=your_token_here
 }
 
-# Define the Portfolio Site
 resource "netlify_site" "sre_portfolio" {
-  name = "neelima-sre-portfolio" 
+  name = "neelima-sre-portfolio-2025" # Make sure this is unique
 
-  # Link to your GitHub Repository
   repo {
     provider    = "github"
-    repo_path   = "Whatever2095/portfolio" 
+    repo_path   = "Whatever2095/portfolio"
     branch      = "main"
-    
-    # The directory where the HTML site lives
-    dir         = "html5up-strata" 
-    
-    # We leave the command empty because it's static HTML
+    dir         = "html5up-strata"
     command     = "" 
   }
 }
 
-# Output the live URL after deployment
 output "live_url" {
   value = netlify_site.sre_portfolio.url
 }
